acc9b055705e8589aa50e174ea13661aabe096bd
Wohlstand <admin@wohlnet.ru>
2025-04-23 01:52:34 +0300

    al_midi.c: Don't include stdio.h
    It was added here temporarily for debug purposes!

--- source/al_midi.c
+++ source/al_midi.c
@@ -30,7 +30,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 **********************************************************************/
 
 #include <conio.h>
-#include <stdio.h>
 #include <dos.h>
 #include <stddef.h>
 #include <stdlib.h>
